77ceb300bfb48036c6ed82981dc0784437ba08dd
ryuzaki02 <thakur.aman77@yahoo.com>
2022-11-29 22:45:14 -0500

    Initial commit

---
+++ TimeTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
