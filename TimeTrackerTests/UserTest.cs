898ab69d77b4b316b6672849ad557929e2bc5161
ryuzaki02 <thakur.aman77@yahoo.com>
2022-11-30 23:58:13 -0500

    Added Test case
    - Created a test class to test the list of user empty or not
    - Added project reference to the project of class library
    - Created object of view model
    - Tested if users list is empty or not

---
+++ TimeTrackerTests/UserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TimeTrackerLibrary.ViewModel;
+using TimeTrackerLibrary.Data;
+using TimeTrackerLibrary.Model;
+
+namespace TimeTrackerTests
+{
+    [TestClass]
+    public class UserTest
+	{
+		private UserViewModel userViewModel;
+		public UserTest()
+		{
+			TimeTrackerDbContext dbContext = new TimeTrackerDbContext();
+			userViewModel = new UserViewModel(dbContext);
+		}
+
+		[TestMethod]
+		public async Task Check_For_Empty_Users_TestAsync()
+		{
+			List<User> users = await userViewModel.GetUsers();
+			Assert.IsTrue(users.Count == 0);
+		}
+	}
+}
+
